3a17642e5b8c0a9c771b9bb1ca1d5b70a678e035
zeiser <yo@yo.yo>
2020-02-07 16:54:39 +0100

    settings stuff webpage

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsPage extends StatefulWidget{
   @override
@@ -8,12 +9,34 @@ class SettingsPage extends StatefulWidget{
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+
+
+  void _connectFitbit() async {
+    const url = 'https://flutter.dev';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Settings"),
       ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: RaisedButton(
+              onPressed: _connectFitbit,
+              child: Text("Connect fitbit")
+            )
+          ),
+        ]
+      )
     );
   }
   
