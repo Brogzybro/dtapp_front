6700c1eadd9a7edfa5b236fce509bf99d9d43693
Asd <Asd>
2020-02-08 17:39:34 +0100

    fitbit sync feature complete maybe

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:openapi/api.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+FitbitApi fitbitApi = FitbitApi();
 class SettingsPage extends StatefulWidget{
   @override
   State<StatefulWidget> createState() => _SettingsPageState();
@@ -12,12 +14,25 @@ class _SettingsPageState extends State<SettingsPage> {
 
 
   void _connectFitbit() async {
-    const url = 'https://flutter.dev';
+    final idk = await fitbitApi.userTokenPost();
+    print(idk.token);
+
+
+    final url = fitbitApi.apiClient.basePath + "/fitbit/auth?token=" + idk.token;
     if (await canLaunch(url)) {
       await launch(url);
     } else {
       throw 'Could not launch $url';
     }
+  }
+
+  Future<bool> _checkFitbitConnection() async {
+    try {
+      return await fitbitApi.fitbitIsauthorizedGet();
+    } catch (e) {
+      print(e);
+    }
+    return false;
   }
 
   @override
@@ -27,14 +42,49 @@ class _SettingsPageState extends State<SettingsPage> {
         title: Text("Settings"),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          Center(
-            child: RaisedButton(
-              onPressed: _connectFitbit,
-              child: Text("Connect fitbit")
+          Container(
+            height: 60,
+            child: Card(
+              child: Container(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Fitbit connection"),
+                    FutureBuilder(
+                      future: _checkFitbitConnection(),
+                      builder: (BuildContext context, snapshot){
+                        if(snapshot.hasData){
+                          if(snapshot.data){
+                            return Container(
+                              child: Row(
+                                children: <Widget>[
+                                  Text("Connected "),
+                                  Icon(Icons.check_circle, color: Colors.green)
+                                ],
+                              ),
+                            );
+                          }else{
+                            return RaisedButton(
+                              onPressed: _connectFitbit,
+                              child: Text("Connect")
+                            );
+                          }
+                        }else if (snapshot.hasError) {
+                          return Text("${snapshot.error}");
+                        }
+                        return CircularProgressIndicator();
+                      }
+                    ),
+                    
+                  ],
+                ),
+              )
+              
             )
-          ),
+          )
         ]
       )
     );
