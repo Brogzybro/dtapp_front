b388509e0807f999d76eae0a670c07b1a3f16640
Asd <Asd>
2020-02-23 22:51:29 +0100

    change

--- lib/samples/sample_views/generic_sample_view.dart
+++ lib/samples/sample_views/generic_sample_view.dart
@@ -1,4 +1,5 @@
 import 'package:dtapp_flutter/samples/sample_views/sample_dialog.dart';
+import 'package:dtapp_flutter/util/date_format.dart';
 import 'package:flutter/material.dart';
 import 'package:openapi/api.dart' hide Type;
 
@@ -11,30 +12,30 @@ class GenericSampleView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => mydialog(context, sample),
-      child: Container(
-        height: 50,
-        color: color,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text('${sample.type.value} : ${(sample.value is double) ? (sample.value as num).toStringAsFixed(4) : sample.value}'),
-                Text("Collected from: ${sample.source_}")
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text("Start date: ${DateTime.fromMillisecondsSinceEpoch(sample.startDate)}"), 
-                Text("End date: ${DateTime.fromMillisecondsSinceEpoch(sample.endDate)}")
-              ],
-            )
-          ]
-        )
-      )
-    );
+        onTap: () => mydialog(context, sample),
+        child: Container(
+            height: 50,
+            color: color,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text(
+                          '${sample.type.value} : ${(sample.value is double) ? (sample.value as num).toStringAsFixed(4) : sample.value}'),
+                      Text("Collected from: ${sample.source_}")
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text(
+                          "Start: ${formatDate(DateTime.fromMillisecondsSinceEpoch(sample.startDate))}"),
+                      Text(
+                          "End: ${formatDate(DateTime.fromMillisecondsSinceEpoch(sample.endDate))}")
+                    ],
+                  )
+                ])));
   }
 }
---
+++ lib/util/date_format.dart
@@ -0,0 +1,3 @@
+String formatDate(DateTime date) {
+  return "${date.year.toString()}-${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')} ${date.hour.toString().padLeft(2, '0')}:${date.minute.toString().padLeft(2, '0')}:${date.second.toString().padLeft(2, '0')}";
+}
