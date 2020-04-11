99cc47aea1e742799ef7f52c5ec47fe2816c56d8
Asd <Asd>
2020-04-11 16:42:44 +0200

    removed action button from login and signup

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -26,14 +26,14 @@ class Model {
 
 class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
-  
+
   Model _model = Model();
 
-  void _incrementCounter() async{
+  void _incrementCounter() async {
     try {
       await userapiInstance.rootGet();
       print("Success token at incr");
-    } catch (e){
+    } catch (e) {
       print(e);
     }
   }
@@ -44,23 +44,19 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(
         title: Text('Welcome to your Digital Twin'),
       ),
-      body: Builder(
-        builder: (BuildContext context) {
-          return Column(
+      body: Builder(builder: (BuildContext context) {
+        return Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: <Widget>[
+                  padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                  child: Form(
+                      key: _formKey,
+                      child: Column(children: <Widget>[
                         Text(
                           "Log in",
                           style: TextStyle(
-                            fontSize: 32,
-                            fontWeight: FontWeight.bold
-                            ),
+                              fontSize: 32, fontWeight: FontWeight.bold),
                         ),
                         TextFormField(
                           decoration: const InputDecoration(
@@ -88,76 +84,61 @@ class _LoginPageState extends State<LoginPage> {
                           obscureText: true,
                         ),
                         RaisedButton(
-                          onPressed: () {
+                            onPressed: () {
+                              Scaffold.of(context).showSnackBar(SnackBar(
+                                  content: Text("Attempting log in..."),
+                                  duration: Duration(minutes: 5)));
 
-                            Scaffold.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text("Attempting log in..."),
-                                duration: Duration(minutes: 5)
-                              )
-                            );
-                            
-                            if (_formKey.currentState.validate()) {
-                              _formKey.currentState.save();
-                              attemptLogin(_model).then((success){
-                                var text = (success) ? 
-                                  "Logged in successfully!" : "Login failed. Check username/password.";
-                                print(text);
-                                Scaffold.of(context).hideCurrentSnackBar();
-                                Scaffold.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: Text(text)
-                                  )
-                                );
-                                if(success){
-                                  Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(builder: (context) => HomePage(title: "test")),
-                                  );     
-                                }
-                              });                       
-                            }
-                            print("yo");
-                          },
-                          child: Text('Log in')
-                        ),
+                              if (_formKey.currentState.validate()) {
+                                _formKey.currentState.save();
+                                attemptLogin(_model).then((success) {
+                                  var text = (success)
+                                      ? "Logged in successfully!"
+                                      : "Login failed. Check username/password.";
+                                  print(text);
+                                  Scaffold.of(context).hideCurrentSnackBar();
+                                  Scaffold.of(context).showSnackBar(
+                                      SnackBar(content: Text(text)));
+                                  if (success) {
+                                    Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              HomePage(title: "test")),
+                                    );
+                                  }
+                                });
+                              }
+                              print("yo");
+                            },
+                            child: Text('Log in')),
                         RaisedButton(
-                          onPressed: () {
-                            // Validate will return true if the form is valid, or false if
-                            // the form is invalid.
-                            //if (_formKey.currentState.validate()) {
+                            onPressed: () {
+                              // Validate will return true if the form is valid, or false if
+                              // the form is invalid.
+                              //if (_formKey.currentState.validate()) {
                               // Process data.
-                            //}'
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(builder: (context) => SignupPage()),
-                            );
-                            print("yo");
-                          },
-                          child: Text('Go to Sign up'),
-                          color: Colors.orangeAccent
-                        )
-                    ]
-                  )
-                )
-              )
-            ]
-          );
-        }
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+                              //}'
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SignupPage()),
+                              );
+                              print("yo");
+                            },
+                            child: Text('Go to Sign up'),
+                            color: Colors.orangeAccent)
+                      ])))
+            ]);
+      }),
     );
   }
 }
 
-Future<bool> attemptLogin(Model model) async{
+Future<bool> attemptLogin(Model model) async {
   print("Attempting login...");
-  HttpBasicAuth bauth = userapiInstance.apiClient.getAuthentication("basicAuth");
+  HttpBasicAuth bauth =
+      userapiInstance.apiClient.getAuthentication("basicAuth");
   bauth.username = model.username;
   bauth.password = model.password;
   try {
@@ -168,7 +149,7 @@ Future<bool> attemptLogin(Model model) async{
     user.password = model.password;
     loggedInUser = user;
     return true;
-  } catch (e){
+  } catch (e) {
     print(e);
     return false;
   }
--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -18,132 +18,111 @@ class SignupPage extends StatefulWidget {
 class _SignupPageState extends State<SignupPage> {
   final _formKey = GlobalKey<FormState>();
   Model _model = new Model();
-  
-  void _actionButtonAction(){
+
+  void _actionButtonAction() {
     print("Useless aciton button pressed");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Sign up to Digital Twin")
-      ),
-      body: Builder(
-        builder: (BuildContext context) {
-          return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      children: <Widget>[
-                          Text(
-                            "Sign up",
-                            style: TextStyle(
-                              fontSize: 32,
-                              fontWeight: FontWeight.bold
-                              ),
-                          ),
-                          TextFormField(
-                            decoration: const InputDecoration(
-                              hintText: 'Enter your username',
-                            ),
-                            validator: (value) {
-                              if (value.isEmpty) {
-                                return 'Please enter some text';
-                              }
-                              return null;
-                            },
-                            onSaved: (val) => _model.username = val,
-                          ),
-                          TextFormField(
-                            decoration: const InputDecoration(
-                              hintText: 'Enter your password',
-                            ),
-                            validator: (value) {
-                              if (value.isEmpty) {
-                                return 'Please enter some text';
-                              }
-                              return null;
-                            },
-                            onSaved: (val) => _model.password = val,
-                            obscureText: true,
-                          ),
-                          RaisedButton(
-                            onPressed: () {
-                              Scaffold.of(context).showSnackBar(
-                                SnackBar(
-                                  content: Text("Attempting sign up..."),
-                                  duration: Duration(minutes: 5)
-                                )
-                              );
-
-                              if (_formKey.currentState.validate()) {
-                                _formKey.currentState.save();
-                                attemptSignup(_model).then((success){
-                                  var text = (success) ? 
-                                    "User created!" : "User creation failed. Username probably taken.";
-                                  print(text);
-                                  Scaffold.of(context).hideCurrentSnackBar();
-                                  Scaffold.of(context).showSnackBar(
-                                    SnackBar(
-                                      content: Text(text)
-                                    )
-                                  );
-
-                                });
-                              }
-                              print("yo");
-                            },
-                            child: Text('Sign up')
-                          ),
-                          RaisedButton(
-                            onPressed: () {
-                              // Validate will return true if the form is valid, or false if
-                              // the form is invalid.
-                              //if (_formKey.currentState.validate()) {
-                                // Process data.
-                              //}'
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(builder: (context) => LoginPage(title: "test")),
-                              );
-                              print("yo");
-                            },
-                            child: Text('Go to log in'),
-                            color: Colors.orangeAccent
-                          ),
-                      ],
-                    ),
+      appBar: AppBar(title: Text("Sign up to Digital Twin")),
+      body: Builder(builder: (BuildContext context) {
+        return Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+            Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "Sign up",
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
                   ),
-                )
-              ]
-            );
-          }
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _actionButtonAction,
-          tooltip: 'Yo',
-          child: Icon(Icons.add)
-        ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Enter your username',
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                    onSaved: (val) => _model.username = val,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Enter your password',
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                    onSaved: (val) => _model.password = val,
+                    obscureText: true,
+                  ),
+                  RaisedButton(
+                      onPressed: () {
+                        Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text("Attempting sign up..."),
+                            duration: Duration(minutes: 5)));
+
+                        if (_formKey.currentState.validate()) {
+                          _formKey.currentState.save();
+                          attemptSignup(_model).then((success) {
+                            var text = (success)
+                                ? "User created!"
+                                : "User creation failed. Username probably taken.";
+                            print(text);
+                            Scaffold.of(context).hideCurrentSnackBar();
+                            Scaffold.of(context)
+                                .showSnackBar(SnackBar(content: Text(text)));
+                          });
+                        }
+                        print("yo");
+                      },
+                      child: Text('Sign up')),
+                  RaisedButton(
+                      onPressed: () {
+                        // Validate will return true if the form is valid, or false if
+                        // the form is invalid.
+                        //if (_formKey.currentState.validate()) {
+                        // Process data.
+                        //}'
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LoginPage(title: "test")),
+                        );
+                        print("yo");
+                      },
+                      child: Text('Go to log in'),
+                      color: Colors.orangeAccent),
+                ],
+              ),
+            ),
+          )
+        ]);
+      }),
     );
   }
 }
 
-Future<bool> attemptSignup(Model model) async{
+Future<bool> attemptSignup(Model model) async {
   print("Signing up...");
   var user = new User();
   user.username = model.username;
   user.password = model.password;
-  try{
+  try {
     var postReq = await userapiInstance.userPost(user);
     print("User created");
     print(postReq);
     return true;
-  } catch(e){
+  } catch (e) {
     print(e);
     return false;
   }
